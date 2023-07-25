70d48cf788117b73c5cf99bbaf467cc8f9053084
Manan837 <89606852+Manan837@users.noreply.github.com>
2023-07-25 12:35:00 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 song="";
+leftWristX=0;
+leftWristY=0;
+rightWristX=0;
+rightWristY=0;
 
 function setup()
 {
@@ -7,6 +11,31 @@ function setup()
 
     video = createCapture(VIDEO);
     video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("Left Wrist X = " + leftWristX + "Left Wrist Y = " + leftWristY);
+
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log(`Right Wrist X = ${rightWristX} \n\ Right Wrist Y = ${rightWristY}`);
+        // Template Literal^
+    }
+}
+
+function modelLoaded()
+{
+    console.log("Model Loaded!");
 }
 
 function draw()
@@ -22,4 +51,6 @@ function preload()
 function play()
 {
     song.play();
+    song.setVolume(1);
+    song.rate(1);
 }
